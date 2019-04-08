d3c968c10b237583eed3026ab2f7c20b636fac7b
Matthew White <Matthew.White02@gmail.com>
2019-04-08 22:19:58 +0100

    Register IRestaurantData with application

--- OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood.Data/IRestaurantData.cs
@@ -1,53 +1,10 @@
 ﻿using OdeToFood.Core;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace OdeToFood.Data
 {
     public interface IRestaurantData
     {
         IEnumerable<Restaurant> GetAll();
-    }
-
-    public class InMemoryRestaurantData : IRestaurantData
-    {
-        readonly List<Restaurant> restaurants;
-
-        public InMemoryRestaurantData()
-        {
-            restaurants = new List<Restaurant> {
-                new Restaurant {
-                    Id = 1,
-                    Name = "Matt's Home Cooking",
-                    Location = "Home",
-                    Cuisine = CuisineType.None,
-                },
-                new Restaurant {
-                    Id = 2,
-                    Name = "Cinnamon Garden",
-                    Location = "Ashbourne",
-                    Cuisine = CuisineType.Indian,
-                },
-                new Restaurant {
-                    Id = 3,
-                    Name = "Golden Dragon",
-                    Location = "Ashbourne",
-                    Cuisine = CuisineType.Chinese,
-                },
-                new Restaurant {
-                    Id = 4,
-                    Name = "Roberto's",
-                    Location = "Ratoath",
-                    Cuisine = CuisineType.Italian,
-                },
-            };
-        }
-
-        IEnumerable<Restaurant> IRestaurantData.GetAll()
-        {
-            return restaurants.OrderBy(r => r.Name);
-        }
     }
 }
---
+++ OdeToFood.Data/InMemoryRestaurantData.cs
@@ -0,0 +1,46 @@
+﻿using OdeToFood.Core;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        readonly List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant> {
+                new Restaurant {
+                    Id = 1,
+                    Name = "Matt's Home Cooking",
+                    Location = "Home",
+                    Cuisine = CuisineType.None,
+                },
+                new Restaurant {
+                    Id = 2,
+                    Name = "Cinnamon Garden",
+                    Location = "Ashbourne",
+                    Cuisine = CuisineType.Indian,
+                },
+                new Restaurant {
+                    Id = 3,
+                    Name = "Golden Dragon",
+                    Location = "Ashbourne",
+                    Cuisine = CuisineType.Chinese,
+                },
+                new Restaurant {
+                    Id = 4,
+                    Name = "Roberto's",
+                    Location = "Ratoath",
+                    Cuisine = CuisineType.Italian,
+                },
+            };
+        }
+
+        IEnumerable<Restaurant> IRestaurantData.GetAll()
+        {
+            return restaurants.OrderBy(r => r.Name);
+        }
+    }
+}
